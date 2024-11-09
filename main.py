9b41f80dd37271db66521db891f659228317b668
Amelia Sinclaire <amelia.sylphie.sinclaire@gmail.com>
2024-11-09 05:05:56 -0500

    Changed main.py to be a cli for the skittlize function.

--- main.py
+++ main.py
@@ -1,46 +1,25 @@
+import argparse
+import os.path
+
 import cv2
+
+# from renderskittle import renderSkittles
 from skittlize import skittlize
-from renderskittle import renderSkittles
 
-def main():
-    # the path of the image to convert (try to pick something small, like 128x128 or less, larger will take time)
-    img = "Examples/color_test.jpg"
-    # green, red, orange, yellow, purple, white (skittle colors - last element is always the "bg" color)
-    skittle_palette = ["#378e30", "#912439", "#dc5343", "#c5ba5e", "#1d1b20", "#ffffff"]
-    rainbow_palette = ['#ff595e', '#ff924c', '#ffca3a', '#c5ca30', '#8ac926', '#52a675', '#1982c4', '#4267ac', '#6a4c93', '#ffffff', '#000000']
-    rainbow_counts  = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    # counts of each color in array above. - last element value doesn't matter
-    skittle_counts = [17, 18, 21, 21, 22, 999]
-    # how we weight the hue, saturation, and value when comparing img col to palette col
-    # the higher it's weighted the more important it is to preserve
-    H = 1
-    S = 1
-    V = 1
-    # this loop just increases the number of each skittle for testing purposes
-    # in a real implementation your counts array should have the real num of skittles available to begin with
-    n_bags = 65
-    for q in range(len(skittle_counts)):
-        skittle_counts[q] *= n_bags
 
-    # this is the line the actually skittlefies your img.
-    # it has an option to make the counts unlimited (will allow you to see what skittlefy would do in the ideal situation)
-    # it has an option to use_all. this will ensure it uses every single skittle you give it
-    #   (provided the number of skittles is smaller than the canvas size)
-    # it also ha a priority option with three choices: none, random, and confidence.
-    # none assigns skittles in pixel order, random in random order, confidence assigns the most confident skittles first
-    out_img, out_arr, out_count = skittlize(img, rainbow_palette, rainbow_counts, hue_weight=H, sat_weight=S, val_weight=V, unlimited=True, use_all=True, priority='confidence')
-    # it returns three values: out_img, out_arr, and out_count
-    # out_img is a cv2 img with the skittle colors in it
-    # out_arr is a 2d array where each element has an int value corresponding to the color that pixel has been assigned
-    #     the value is the index of the color in the skittle_palette you provided
-    # out_count is the same skittle_count you passed in, but with new values representing how many skittles are remaining
-    #     if you have use_all=True, they should all = 0
+def main(args):
+    i = args.image_path
+    p = args.palette
+    c = args.counts
+    h = args.hue_weight
+    s = args.sat_weight
+    v = args.val_weight
+    u = args.unlimited
+    a = args.use_all
+    o = args.order
+    out_img, out_arr, out_count = skittlize(i, p, c, h, s, v, u, a, o)
 
-    # displaying some of this information:
-    # prints skittle counts before and after
-    print(skittle_counts)
-    print(out_count)
-
+    # TODO: add render skittle back in?
     # Take the returned array and the palette to create an image where the pixels
     # represent a skittle and draw a circle instead of the pixel. Each circle
     # will be colored using the palette.
@@ -50,13 +29,69 @@ def main():
     # renderSkittles(circDiam, cols, rows, out_arr, skittle_palette)
 
     # displays before and after images
-    img = cv2.imread(img)
+    img = cv2.imread(i)
     cv2.imshow("before", img)
-    cv2.waitKey(1)
     cv2.imshow("after", out_img)
-    cv2.waitKey(0)
+
+    while cv2.getWindowProperty('before', cv2.WND_PROP_VISIBLE) >=1 and\
+            cv2.getWindowProperty('after' , cv2.WND_PROP_VISIBLE) >=1:
+        keyCode = cv2.waitKey(1)  # https://medium.com/@mh_yip/opencv-detect-whether-a-window-is-closed-or-close-by-press-x-button-ee51616f7088
+        if (keyCode & 0xFF) == ord("q"):
+            cv2.destroyAllWindows()
+            break
     cv2.destroyAllWindows()
 
 
 if __name__=='__main__':
-    main()
+    # TODO: change the default palette and counts to skittle versions?
+    rainbow_palette = ['#ff595e', '#ff924c', '#ffca3a', '#c5ca30', '#8ac926',
+                       '#52a675', '#1982c4', '#4267ac', '#6a4c93', '#ff0000',
+                       '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff',
+                       '#ffffff', '#000000']
+    rainbow_counts = [500] * len(rainbow_palette)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image_path',
+                        type=str,
+                        help='<Requited> Path of image to be skittlized.')
+    parser.add_argument('-p', '--palette',
+                        nargs='+',
+                        type=str,
+                        default=rainbow_palette,
+                        help='List of hex codes to use. The last color is the background color.')
+    parser.add_argument('-c', '--counts',
+                        nargs='+',
+                        type=int,
+                        default=rainbow_counts,
+                        help='List of counts for each color. If left untouched it will default to 500 per color. The last color is the background color, as the count is infinite no matter what you put.')
+    parser.add_argument('-x', '--hue_weight',
+                        type=float,
+                        default=1.0,
+                        help='Hue weight. Used to preserve hue.')
+    parser.add_argument('-s', '--sat_weight',
+                        type=float,
+                        default=1.0,
+                        help='Saturation weight, Used to preserve saturation.')
+    parser.add_argument('-v', '--val_weight',
+                        type=float,
+                        default=1.0,
+                        help='Value weight. Used to preserve value.')
+    parser.add_argument('-u', '--unlimited',
+                        type=bool,
+                        default=False,
+                        help='If true the counts will be irrelevant. Will treat as though you have infinite of each')
+    parser.add_argument('-a', '--use_all',
+                        type=bool,
+                        default=False,
+                        help='If true the program will attempt to use every count of every color given.')
+    parser.add_argument('-o', '--order',
+                        type=str,
+                        default='confidence',
+                        help='Priority order to place the colors. [random, pixel, confidece]. Random places randomly. Pixel places in pixel order. Confidence places the most confident pixels first.')
+    args = parser.parse_args()
+    if not os.path.exists(args.image_path):
+        raise ValueError(f'{args.image_path} does not exist.')
+    if args.counts == rainbow_counts and args.palette != rainbow_palette:
+        args.counts = [500] * len(args.palette)
+
+    main(args)
